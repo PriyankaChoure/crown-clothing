cc9b1c22c97a32f8043f86108daa6063ac2ce6f3
Priyanka <priyanka@Priyankas-MacBook-Pro.local>
2022-11-08 15:41:28 +0530

    create link in home page for each category

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './directory-item.style.scss';
 
 const DirectoryItem = ({category}) =>{
@@ -5,10 +6,10 @@ const DirectoryItem = ({category}) =>{
 return (
     <div className="directory-item-container">
           <div className="background-image" style={{ backgroundImage: `url(${imageUrl})`}} ></div>
-          <div className="body">
+          <Link to={`/shop/${title}`}  className="body">
             <h2>{title}</h2>
             <p>Shop Now</p>
-          </div>
+          </Link>
         </div>
 )
 
